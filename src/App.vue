c2e9a6eeff1ea53262c9c7d0c6d789238b80294f
LiShuxue <1149926505@qq.com>
2022-10-11 21:04:49 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import HelloWorld from '@/components/HelloWorld.vue';
+</script>
+
+<template>
+  <HelloWorld msg="hello world" />
+
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </nav>
+
+  <router-view />
+</template>
+
+<style lang="scss" scoped>
+@import '@/assets/base.scss';
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { useCounterStore } from '@/stores/counter';
+const counter = useCounterStore();
+
+counter.count++;
+// 带自动补全 ✨
+counter.$patch({ count: counter.count + 1 });
+// 或使用 action 代替
+counter.increment();
+</script>
+
+<template>
+  <div class="home">
+    <h1>This is home page</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
